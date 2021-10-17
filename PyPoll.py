c577f420d19a4826be61492ee9e4fffba8b89c4b
coleherman370 <cole.herman370@outlook.com>
2021-10-16 23:47:43 -0500

    Update PyPoll.py: 3.5.2

--- PyPoll.py
+++ PyPoll.py
@@ -10,14 +10,17 @@ import os
 
 file_to_load = os.path.join("Resources", "election_results.csv")
 file_to_save = os.path.join("Analysis", "election_analysis.txt")
+total_votes = 0
 
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
     headers = next(file_reader)
-    print(headers)
+    
+    for row in file_reader:
+        total_votes += 1
+    print(total_votes)
 
-#with open(file_to_save,'w') as txt_file:
-     #txt_file.write("Counties in the Election\n------------------------\nArapahoe\nDenver\nJefferson")
+
 
 
