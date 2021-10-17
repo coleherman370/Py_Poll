3fd4ef25230fd75e04159968c126f2c27c647cba
coleherman370 <cole.herman370@outlook.com>
2021-10-16 23:51:41 -0500

    Update PyPoll.py: 3.5.2

--- PyPoll.py
+++ PyPoll.py
@@ -11,6 +11,7 @@ import os
 file_to_load = os.path.join("Resources", "election_results.csv")
 file_to_save = os.path.join("Analysis", "election_analysis.txt")
 total_votes = 0
+candidate_options = []
 
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
@@ -19,7 +20,11 @@ with open(file_to_load) as election_data:
     
     for row in file_reader:
         total_votes += 1
-    print(total_votes)
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+    
+    print(candidate_options)
 
 
 
