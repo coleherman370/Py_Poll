ba05a2de3c36a726f11923bb281cdf8ae08dc439
coleherman370 <cole.herman370@outlook.com>
2021-10-17 00:42:15 -0500

    Update PyPoll.py: 3.6.2

--- PyPoll.py
+++ PyPoll.py
@@ -17,48 +17,56 @@ winning_candidate = ""
 winning_count = 0
 winning_percentage = 0
 
-with open(file_to_load) as election_data:
-    file_reader = csv.reader(election_data)
+with open (file_to_save,'w') as txt_file:
 
-    headers = next(file_reader)
-    
-    for row in file_reader:
-        total_votes += 1
-        candidate_name = row[2]
-        if candidate_name not in candidate_options:
-            candidate_options.append(candidate_name)
-            candidate_votes[candidate_name] = 0
+    with open(file_to_load) as election_data:
+
+        file_reader = csv.reader(election_data)
+        header = next(file_reader)
         
-        candidate_votes[candidate_name] += 1
-    
-    with open (file_to_save,'w') as txt_file:
+        for row in file_reader:
+            total_votes += 1
+            candidate_name = row[2]
+
+            if candidate_name not in candidate_options:
+                candidate_options.append(candidate_name)
+                candidate_votes[candidate_name] = 0   
+
+            candidate_votes[candidate_name] += 1
+        
         election_results = (
             f"\nElection Results\n"
             f"-------------------------\n"
             f"Total Votes: {total_votes:,}\n"
-            f"-------------------------\n")
+            f"-------------------------\n")               
         
-        print(election_results, end="")
+        print(candidate_votes)
         txt_file.write(election_results)
+
+        for candidate_name in candidate_votes:
+
+            votes = candidate_votes[candidate_name]
+            vote_percentage = round(float(votes)/float(total_votes) * 100,1)     
+            candidate_results =  f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n"
+            txt_file.write(candidate_results)      
             
-    for candidate_name in candidate_votes:
-        votes = candidate_votes[candidate_name]
-        vote_percentage = round(float(votes)/float(total_votes) * 100,1)
-        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+            if (votes > winning_count) and (vote_percentage > winning_percentage):
+                winning_count = votes
+                winning_percentage = vote_percentage
+                winning_candidate = candidate_name   
+
+                winning_candidate_summary = (
+                    f"-------------------------\n"
+                    f"Winner: {winning_candidate}\n"
+                    f"Winning Vote Count: {winning_count:,}\n"
+                    f"Winning Percentage: {winning_percentage:.1f}%\n"
+                    f"-------------------------\n")
         
-        if (votes > winning_count) and (vote_percentage > winning_percentage):
-            winning_count = votes
-            winning_percentage = vote_percentage
-            winning_candidate = candidate_name       
+        txt_file.write(winning_candidate_summary)
         
-            winning_candidate_summary = (
-                f"-------------------------\n"
-                f"Winner: {winning_candidate}\n"
-                f"Winning Vote Count: {winning_count:,}\n"
-                f"Winning Percentage: {winning_percentage:.1f}%\n"
-                f"-------------------------\n")
         
-    print(winning_candidate_summary)
+        # txt_file.write(candidate_results)
+            
 
 
 
